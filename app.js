393a4f4127332f9477095c1b54315d511be3aa34
Jeff Everhart <Jeffeverhart383@gmail.com>
2017-06-12 15:39:38 -0400

    push local changes

---
+++ app.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+	el: '#app', 
+	data: {
+		message: 'Hello Vue!'
+	}
+})
+
+
+var audio = document.querySelector('#audio'); 
+
+audio.addEventListener('play', function(event){
+	console.log(this.currentTime); 
+	console.log(event); 
+
+})
+
+var button = document.querySelector('#annotation-button'); 
+
+button.addEventListener('click', function(){
+
+	var currentAnnotations; 
+	//get current annotations 
+	currentAnnotations = JSON.parse(localStorage.getItem('annotations')); 
+	console.log(currentAnnotations); 
+
+	if (!currentAnnotations){
+		currentAnnotations = []; 
+	}
+
+	var textArea = document.querySelector('#annotation-text'); 
+
+
+	var newAnnotation = {
+		time: '', 
+		text: ''
+	}
+
+	var currentAudioTime = audio.currentTime; 
+	var annotationText = textArea.value;
+
+	newAnnotation.time = currentAudioTime; 
+	newAnnotation.text = annotationText; 
+
+	currentAnnotations.push(newAnnotation); 
+
+	localStorage.setItem('annotations', JSON.stringify(currentAnnotations));  
+
+	textArea.value = ''; 
+
+}); 
