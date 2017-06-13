1e67d22988d15ed4fb3f681b73bab02582a940f5
Jeff Everhart <Jeffeverhart383@gmail.com>
2017-06-13 09:33:19 -0400

    add ui for custom controls

--- peaks-demo.js
+++ peaks-demo.js
@@ -1,13 +1,22 @@
   
 (function(Peaks){
-  var myAudioContext = new AudioContext(); 
+  var audiography = {
+    addSegmentToWaveform: function(segmentID, segmentDescription){
+
+      //this method will add the segment to the waveform with 
+      //edit = true
+
+    }, 
+    saveSegmentToWaveform: function(segmentID){
+      //this method will take the segmentID and update the startTime, endTime, and 
+      //push all changes to the backend
+    }
+  }
 
   var p = Peaks.init({
   	container: document.querySelector('#waveform-container'), 
   	mediaElement: document.querySelector('#audio'), 
-  	// audioContext: myAudioContext
   	dataUri: {
-  		// arrayBuffer: 'sonata.dat'
   		json: 'sonata.json'
   	}, 
     zoomLevels: [512, 1024, 2048, 4096], 
@@ -25,6 +34,8 @@
     console.log(p.segments.getSegments())
   
   })
+
+
 
 
 })(peaks)
